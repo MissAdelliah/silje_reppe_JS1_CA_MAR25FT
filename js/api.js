5dbbe32ba84c6e259fbc9da581a0d0f674116efb
MissAdelliah <siljeandrea@outlook.com>
2025-12-22 22:49:25 +0100

    trying to find out checkout pge fetchone

--- js/api.js
+++ js/api.js
@@ -1,24 +1,3 @@
-/* First try on fetching Api data
-const API_KEY = "https://v2.api.noroff.dev/gamehub";
-const API_URL_PRODUCTS = `${API_URL}/gamehub`;
-
-export async function getAllProductData() {
-  const url = API_KEY;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Failed to load game data");
-    }
-
-    const result = await response.json();
-    console.log(result.data);
-    return result.data; //Returns array of products
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-  */
-
 const ENDPOINT = "https://v2.api.noroff.dev/gamehub";
 
 // fetches API data
